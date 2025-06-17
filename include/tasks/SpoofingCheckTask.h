6ee621cf53e41bff676c7333929c38c919a4cad2
Myakita <146758985+Myakita@users.noreply.github.com>
2025-06-17 21:48:47 +0500

    123

--- include/tasks/SpoofingCheckTask.h
+++ include/tasks/SpoofingCheckTask.h
@@ -1,8 +1,9 @@
 #ifndef SPOOFING_CHECK_TASK_H
 #define SPOOFING_CHECK_TASK_H
 #include "GPSTask.h"
+#include "tasks/Task.h"
 
-class SpoofingCheckTask {
+class SpoofingCheckTask: public Task {
 public:
     static void run(void* pvParameters);
 };
